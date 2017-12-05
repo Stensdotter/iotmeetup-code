5d296a2d7a08560d9d5e850b75aed6edfe45cb20
Mia <Stensdotter@users.noreply.github.com>
2017-12-05 15:06:27 +0100

    Updated code

--- posix/bin/iotclient.c
+++ posix/bin/iotclient.c
@@ -54,7 +54,7 @@ const int sensor_type = 22;
 const int gpio_pin = 4;
 // This is the URN of your device model
 const char* device_urns[] = {
-  "urn:com:oracle:demo:esensor",
+  "urn:com:oracle:demo:mia",
   NULL
 };
 
@@ -85,7 +85,73 @@ int main(int argc, char** argv) {
   fprintf(stderr,"iotcs: iotclient starting!\n");
   fprintf(stderr,"iotcs: Device Urn: %s\n", device_urns[0]);
   fprintf(stderr,"iotcs: Loading configuration from: %s\n", ts_path);
+/*
+ * Initialize the library before any other calls.
+ * Initiate all subsystems like ssl, TAM, request dispatcher,
+ * async message dispatcher, etc which needed for correct library work.
+ */
 
+if (iotcs_init(ts_path, ts_password) != IOTCS_RESULT_OK) {
+    error("Initialization failed");
+}
+/*
+ * Activate the device, if it's not already activated.
+ * Always check if the device is activated before calling activate.
+ * The device model URN is passed into the activate call to tell
+ * the server the device model(s) that are supported by this
+ * device
+ */
+
+if (!iotcs_is_activated()) {
+    if (iotcs_activate(device_urns) != IOTCS_RESULT_OK) {
+        error("Sending activation request failed");
+    }
+}
+/* get device model handle */
+if (iotcs_get_device_model_handle(device_urns[0], &device_model_handle) != IOTCS_RESULT_OK) {
+    fprintf(stderr,"iotcs_get_device_model_handle method failed\n");
+    return IOTCS_RESULT_FAIL;
+}
+
+/* get device handle */
+if (iotcs_get_virtual_device_handle(iotcs_get_endpoint_id(), device_model_handle, &device_handle) != IOTCS_RESULT_OK) {
+    fprintf(stderr,"iotcs_get_device_handle method failed\n");
+    return IOTCS_RESULT_FAIL;
+}
+// Read values from the sensor.
+fprintf(stderr,"iotcs: Reading from the DHT%u sensor!\n", sensor_type);
+result = pi_2_dht_read(sensor_type, gpio_pin, &humidity, &temperature);
+if (result != DHT_SUCCESS) {
+  fprintf(stderr,"iotcs: Warning, Bad data from the DHT%u sensor\n", sensor_type);
+}
+fprintf(stderr,"iotcs: temperature = %2.2f, humidity= %2.2f\n", temperature, humidity);
+// Start setting attribute for IOT
+iotcs_virtual_device_start_update(device_handle);
+// Set attribute
+rv = iotcs_virtual_device_set_float(device_handle, "temperature", temperature);
+if (rv != IOTCS_RESULT_OK) {
+  fprintf(stderr,"iotcs_virtual_device_set_float 1 failed\n");
+  return IOTCS_RESULT_FAIL;
+}
+
+// Set attribute
+rv = iotcs_virtual_device_set_float(device_handle, "humidity", humidity);
+if (rv != IOTCS_RESULT_OK) {
+  fprintf(stderr,"iotcs_virtual_device_set_float 2 failed\n");
+  return IOTCS_RESULT_FAIL;
+}
+// We are done. IOT can sync the virtual device
+iotcs_virtual_device_finish_update(device_handle);
+/* free device handle */
+iotcs_free_virtual_device_handle(device_handle);
+/* free device model handle */
+iotcs_free_device_model_handle(device_model_handle);
+
+/*
+ * Calling finalization of the library ensures communications channels are closed,
+ * previously allocated temporary resources are released.
+ */
+iotcs_finalize();
 
 /*
 *
